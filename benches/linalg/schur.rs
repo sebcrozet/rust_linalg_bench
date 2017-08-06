ef78849317257c6cc93f007c6e2d355cd75c4404
Sébastien Crozet <developer@crozet.re>
2017-08-06 19:44:00 +0200

    Add schur decomposition benchmark.

---
+++ benches/linalg/schur.rs
@@ -0,0 +1,43 @@
+use test::{self, Bencher};
+use na::RealSchur;
+use nl;
+
+
+/*
+ *
+ *
+ * 100x100
+ *
+ *
+ */
+#[bench]
+fn schur_100x100_na(bh: &mut Bencher) {
+    let m = ::reproductible_dmatrix_na(100, 100);
+    bh.iter(|| test::black_box(RealSchur::new(m.clone())))
+}
+
+#[bench]
+fn schur_100x100_na_lapack(bh: &mut Bencher) {
+    let m = ::reproductible_dmatrix_na(100, 100);
+    bh.iter(|| test::black_box(nl::RealSchur::new(m.clone())))
+}
+
+
+/*
+ *
+ *
+ * 200x200
+ *
+ *
+ */
+#[bench]
+fn schur_200x200_na(bh: &mut Bencher) {
+    let m = ::reproductible_dmatrix_na(200, 200);
+    bh.iter(|| test::black_box(RealSchur::new(m.clone())))
+}
+
+#[bench]
+fn schur_200x200_na_lapack(bh: &mut Bencher) {
+    let m = ::reproductible_dmatrix_na(200, 200);
+    bh.iter(|| test::black_box(nl::RealSchur::new(m.clone())))
+}
